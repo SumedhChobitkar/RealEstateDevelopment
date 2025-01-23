f60b6eee7246ba831af587eab284c901057d7e73
SumedhChobitkar <158187477+SumedhChobitkar@users.noreply.github.com>
2025-01-23 15:36:43 +0530

    Add files via upload

---
+++ RealEstateDevelopment/src/main/java/com/RealEstateDevelopment/RealEstateDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.RealEstateDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateDevelopmentApplication.class, args);
+	}
+
+}
